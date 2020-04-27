3151197e2d2d794831e8dbeb9f65021e861954df
ctrlMarcio <zmduartez@gmail.com>
2020-04-27 13:44:58 +0000

    Deploying to gh-pages from  @ b765d71e72188118e788c9aecefc2ddcef059429 🚀

---
+++ search/defines_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['args_5ferror',['ARGS_ERROR',['../error_8h.html#a6478e40bba1979e3815c79c039af2f2f',1,'error.h']]]
+];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,12 +1,21 @@
 var indexSectionsWithContent =
 {
+  0: "ae",
+  1: "e",
+  2: "a"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "files",
+  2: "defines"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Files",
+  2: "Macros"
 };
 
