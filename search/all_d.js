557a0fd10f2a3ceeb5aab5a8222eafdfac1ac295
luist18 <34719043+luist18@users.noreply.github.com>
2020-05-15 13:55:08 +0000

    Deploying to gh-pages from  @ 320ca7b10c34f0cb5fb3abe4d46d54abe93f4d4a 🚀

--- search/all_d.js
+++ search/all_d.js
@@ -7,6 +7,7 @@ var searchData=
   ['server_5ffifo_2eh',['server_fifo.h',['../server__fifo_8h.html',1,'']]],
   ['server_5fthread_2eh',['server_thread.h',['../server__thread_8h.html',1,'']]],
   ['set_5fbathroom_5finfo',['set_bathroom_info',['../server__thread_8h.html#ab20e7ac2ae7cbdf3375726ba565042fc',1,'server_thread.c']]],
+  ['set_5frequests',['set_requests',['../server__thread_8h.html#a78ac8301dd8a9b41db973f195c53cf6d',1,'server_thread.c']]],
   ['setup_5fsignals',['setup_signals',['../client_8c.html#aa7cf6f40dbd3995813e1b3769b001f07',1,'client.c']]],
   ['sigint_5fhandler',['sigint_handler',['../server_8h.html#a4b515337891f74ebd79cb307db16bf33',1,'server.c']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['set_5fbathroom_5finfo',['set_bathroom_info',['../server__thread_8h.html#ab20e7ac2ae7cbdf3375726ba565042fc',1,'server_thread.c']]],
+  ['set_5frequests',['set_requests',['../server__thread_8h.html#a78ac8301dd8a9b41db973f195c53cf6d',1,'server_thread.c']]],
   ['setup_5fsignals',['setup_signals',['../client_8c.html#aa7cf6f40dbd3995813e1b3769b001f07',1,'client.c']]],
   ['sigint_5fhandler',['sigint_handler',['../server_8h.html#a4b515337891f74ebd79cb307db16bf33',1,'server.c']]]
 ];
